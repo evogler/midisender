bc71c29d7f774414edea247b086293cfbf31b85a
Eric Vogler <ericvogler@gmail.com>
2022-02-06 13:05:44 -0800

    add timeout management

--- playmidi.js
+++ playmidi.js
@@ -15,21 +15,28 @@ var bufferIncrement = 100; // milliseconds
 var now = function () { return (function (t) { return (t[0] + t[1] / 1e9) * 1000; })(process.hrtime()); };
 var overallStartTime = now();
 var latestEndingNote = now();
-var scheduleNote = function (note, time0) {
+var timeouts = new Map();
+var scheduleNote = function (note, time0, noteId) {
     var startTime = realTime(data.bpm)(note.time) + time0 + bufferSize;
     var endTime = realTime(data.bpm)(note.time + note.duration) + time0 + bufferSize;
     var pitch = note.pitch, channel = note.channel, velocity = note.velocity;
-    setTimeout(function () {
+    timeouts.set([noteId, "noteon"], setTimeout(function () {
+        timeouts["delete"]([noteId, "noteon"]);
         if (!playing)
             return;
         output.send("noteon", { note: pitch, channel: channel, velocity: velocity });
         latestEndingNote = Math.max(latestEndingNote, endTime);
-    }, startTime - now());
-    setTimeout(function () {
+    }, startTime - now()));
+    timeouts.set([noteId, "noteoff"], setTimeout(function () {
+        timeouts["delete"]([noteId, "noteoff"]);
         if (!playing)
             return;
         output.send("noteoff", { note: pitch, channel: channel, velocity: velocity });
-    }, endTime - now());
+    }, endTime - now()));
+};
+var cancelTimeouts = function () {
+    timeouts.forEach(function (timeout) { return clearTimeout(timeout); });
+    timeouts.clear();
 };
 var killAllNotes = function () {
     for (var reps = 0; reps < 1; reps++) {
@@ -55,7 +62,7 @@ var scheduleNotes = function (notePos, timeWindowStart) {
     while (notePos < data.notes.length &&
         realTime(data.bpm)(data.notes[notePos].time) < timeWindowEnd) {
         var note = data.notes[notePos];
-        scheduleNote(note, overallStartTime);
+        scheduleNote(note, overallStartTime, notePos);
         notePos += 1;
     }
     if (notePos < data.notes.length) {
