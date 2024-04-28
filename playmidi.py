34bcce23852cbdf67c32368fa1041a6b84c48148
Eric Vogler <ericvogler@gmail.com>
2024-04-28 11:05:00 -0700

    chore: remove unneeded files

--- playmidi.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import json
-
-def writeToFileAndPlay(data, filename="notes.json"):
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=2)
-    subprocess.run(['node', 'dist/main.js', filename])
